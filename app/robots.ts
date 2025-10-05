c3158a5558cc5a78baf21c57ac2952566d70c3d8
Popcone <56566419+popcone@users.noreply.github.com>
2025-10-05 16:59:36 -0700

    feat(seo): update robots.txt configuration

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,10 @@
 import type { MetadataRoute } from 'next'
-import { WEBSITE_URL } from '@/lib/constants'
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
+      disallow: '/',
     },
-    sitemap: `${WEBSITE_URL}/sitemap.xml`,
   }
 }
