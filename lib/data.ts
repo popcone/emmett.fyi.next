eda12d02af4434fb2a8b3bb16fae4298d8647d9d
Popcone <56566419+popcone@users.noreply.github.com>
2025-11-30 10:00:59 -0800

    refactor(data): simplify API URL configuration and remove commented code

--- lib/data.ts
+++ lib/data.ts
@@ -1,28 +1,5 @@
-// export const getProjects = async () => {
-//   const url = `${process.env.DATA_API_URL}/items/portfolio_projects?fields=*.*`
-//   const response = await fetch(url)
-//   if (!response.ok) {
-//     throw new Error(`Failed to fetch projects: ${response.statusText}`)
-//   }
-//   const data = await response.json()
-//   return data.data || []
-// }
-
-// export const getTechnologies = async () => {
-//   const url = `${process.env.DATA_API_URL}/items/portfolio_technologies?fields=id,technology`
-//   const response = await fetch(url)
-//   if (!response.ok) {
-//     throw new Error(`Failed to fetch technologies: ${response.statusText}`)
-//   }
-//   const data = await response.json()
-//   return data.data || []
-// }
-
 export const getProjects = async () => {
-  // const url = `${process.env.DATA_URL}`
-
-  // Local CMS - UNCOMMENT THIS WHEN USING LOCAL CMS
-  const url = `${process.env.DATA_API_URL}/items/portfolio_projects?fields=*.*&filter[active][_eq]=true`
+  const url = `${process.env.DATA_URL}`
 
   try {
     const response = await fetch(url)
@@ -35,10 +12,7 @@ export const getProjects = async () => {
 }
 
 export const getTechnologies = async () => {
-  // const url = `${process.env.TECHNOLOGIES_URL}`
-
-  // Local CMS - UNCOMMENT THIS WHEN USING LOCAL CMS
-  const url = `${process.env.DATA_API_URL}/items/portfolio_technologies?fields=id,technology`
+  const url = `${process.env.TECHNOLOGIES_URL}`
 
   try {
     const response = await fetch(url)
